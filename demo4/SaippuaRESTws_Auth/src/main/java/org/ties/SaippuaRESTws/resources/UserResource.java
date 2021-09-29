cdd46f297062e31267ecd16447d87dd858797bc8
stoutcode <mika.riepponen@gmail.com>
2021-09-29 16:20:47 +0300

    Make accordingly changes to users also

--- demo4/SaippuaRESTws_Auth/src/main/java/org/ties/SaippuaRESTws/resources/UserResource.java
+++ demo4/SaippuaRESTws_Auth/src/main/java/org/ties/SaippuaRESTws/resources/UserResource.java
@@ -1,9 +1,7 @@
 package org.ties.SaippuaRESTws.resources;
 
 import java.net.URI;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -12,6 +10,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -28,85 +27,70 @@ public class UserResource {
 	UserService userService = new UserService();
 	
 	@GET
+	@Produces(MediaType.APPLICATION_JSON)
 	public Response getUsers(@Context UriInfo uriInfo) {
-		Map<Object, Object> reply = new LinkedHashMap<>();
 		List<User> returnUsers= userService.getUsers();
 		
 		if (returnUsers == null) {
 			throw new DataNotFoundException();
-		} else {
-			reply.put("Users", returnUsers);
 		}
 		
 		URI uri = uriInfo.getAbsolutePathBuilder().build();
-		return Response.ok(uri).entity(reply).build();
+		return Response.ok(uri).entity(returnUsers).build();
 	}
 	
 	@GET
 	@Path("/{username}")
+	@Produces(MediaType.APPLICATION_JSON)
 	public Response getUsers(@PathParam("username") String username, @Context UriInfo uriInfo) {
-		Map<Object, Object> reply = new LinkedHashMap<>();
 		User returnUser = userService.getUserByUsername(username);
 		
 		if (returnUser == null) {
 			throw new DataNotFoundException();
-		} else {
-			reply.put("User", returnUser);
 		}
 		
 		URI uri = uriInfo.getAbsolutePathBuilder().build();
-		return Response.ok(uri).entity(reply).build();
+		return Response.ok(uri).entity(returnUser).build();
 	}
 	
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response addUser(User user, @Context UriInfo uriInfo) {
-		Map<Object, Object> reply = new LinkedHashMap<>();
 		User returnUser = userService.addUser(user);
 		
 		if (returnUser == null) {
 			throw new CreateException();
-		} else {
-			
-			addUserLinks(uriInfo, returnUser);
-			reply.put("User", returnUser);
 		}
-
+		
+		addUserLinks(uriInfo, returnUser);
 		URI uri = uriInfo.getAbsolutePathBuilder().build();
-		return Response.created(uri).entity(reply).build();
+		return Response.created(uri).entity(returnUser).build();
 	}
 	
 	@PUT
 	@Consumes(MediaType.APPLICATION_JSON)
     public Response updateTaskTeam(User user, @Context UriInfo uriInfo) {
-		Map<Object, Object> reply = new LinkedHashMap<>();
 		User returnUser = userService.changeUser(user);
 
 		if (returnUser == null) {
 			throw new UpdateException("Could not update the user.");
-		} else {
-			addUserLinks(uriInfo, returnUser);
-			reply.put("User", returnUser);
 		}
-
+		
+		addUserLinks(uriInfo, returnUser);
 		URI uri = uriInfo.getAbsolutePathBuilder().build();
-		return Response.created(uri).entity(reply).build();
+		return Response.created(uri).entity(returnUser).build();
     }
 	
 	@DELETE
 	@Path("/{username}")
 	public Response deleteUser(@PathParam("username") String username, @Context UriInfo uriInfo) {
-		Map<Object, Object> reply = new LinkedHashMap<>();
 		User returnUser = userService.deleteUser(username);
 		
 		if (returnUser == null) {
 			throw new DataNotFoundException();
-		} else {
-			reply.put("User", returnUser);
 		}
 		
-		URI uri = uriInfo.getAbsolutePathBuilder().build();
-		return Response.ok(uri).entity(reply).build();
+		return Response.ok().build();
 	}
 	
 	
