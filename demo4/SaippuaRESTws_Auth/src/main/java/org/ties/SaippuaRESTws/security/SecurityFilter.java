82c4928c778dd0e564707d0baf72eadfbe69d35f
stoutcode <mika.riepponen@gmail.com>
2021-10-05 11:14:28 +0300

    refactor if-else to use returns

--- demo4/SaippuaRESTws_Auth/src/main/java/org/ties/SaippuaRESTws/security/SecurityFilter.java
+++ demo4/SaippuaRESTws_Auth/src/main/java/org/ties/SaippuaRESTws/security/SecurityFilter.java
@@ -15,17 +15,22 @@ public class SecurityFilter implements ContainerRequestFilter {
 		
 		if ( requestContext.getUriInfo().getPath().contains("users") ) {
 			secFuncs.handleAuth(requestContext, Arrays.asList("admin"));
-			
-		} else if (requestContext.getMethod().equals("GET")) {
 			return;
-			
-		} else if ( requestContext.getUriInfo().getPath().contains("snippet") ) {
+		}
+		
+		if (requestContext.getMethod().equals("GET")) {
+			return;
+		}
+
+		if ( requestContext.getUriInfo().getPath().contains("snippet") ) {
 			secFuncs.handleAuth(requestContext, Arrays.asList("worker", "manager", "admin"));
-			
-		} else if ( (requestContext.getUriInfo().getPath().contains("languages") || requestContext.getUriInfo().getPath().contains("tasks"))
+			return;
+		}
+		
+		if ( (requestContext.getUriInfo().getPath().contains("languages") || requestContext.getUriInfo().getPath().contains("tasks"))
 				&& ( requestContext.getMethod().equals("DELETE") || requestContext.getMethod().equals("POST") || requestContext.getMethod().equals("PUT") ) ) {
 			secFuncs.handleAuth(requestContext, Arrays.asList("manager", "admin"));
-			
+			return;
 		}
 		
 	}
